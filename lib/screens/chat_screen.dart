f4f28aee73c45e8debd3e120545486be01fe6220
André <andre2ar@outlook.com>
2020-04-16 09:19:34 -0300

    Messages style

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flash_chat/widgets/message_bubble.dart';
 
 class ChatScreen extends StatefulWidget {
   static String id = 'chat_screen';
@@ -25,7 +26,7 @@ class _ChatScreenState extends State<ChatScreen> {
   void getCurrentUser() async {
     try {
       final user = await _auth.currentUser();
-      if(user != null) {
+      if (user != null) {
         loggedInUser = user;
         print(loggedInUser.email);
       }
@@ -57,8 +58,8 @@ class _ChatScreenState extends State<ChatScreen> {
           children: <Widget>[
             StreamBuilder<QuerySnapshot>(
               stream: _firestore.collection('messages').snapshots(),
-              builder: (context, snapshot){
-                if(snapshot.hasData) {
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
                   return Center(
                     child: CircularProgressIndicator(
                       backgroundColor: Colors.lightBlueAccent,
@@ -67,17 +68,21 @@ class _ChatScreenState extends State<ChatScreen> {
                 }
 
                 final messages = snapshot.data.documents;
-                List<Text> messageWidgets = [];
+                List<MessageBubble> messageWidgets = [];
                 for (var message in messages) {
                   final messageText = message.data['text'];
                   final messageSender = message.data['sender'];
 
-                  final messageWidget = Text('$messageText from $messageSender');
+                  final messageWidget = MessageBubble(
+                    text: messageText,
+                    sender: messageSender,
+                  );
                   messageWidgets.add(messageWidget);
                 }
 
                 return Expanded(
                   child: ListView(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
                     children: messageWidgets,
                   ),
                 );
@@ -98,10 +103,8 @@ class _ChatScreenState extends State<ChatScreen> {
                   ),
                   FlatButton(
                     onPressed: () {
-                      _firestore.collection('messages').add({
-                        'sender': loggedInUser.email,
-                        'text': messageText
-                      });
+                      _firestore.collection('messages').add(
+                          {'sender': loggedInUser.email, 'text': messageText});
                     },
                     child: Text(
                       'Send',
---
+++ lib/widgets/message_bubble.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MessageBubble extends StatelessWidget {
+  MessageBubble({this.sender, this.text});
+  final String sender, text;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Text(sender, style: TextStyle(fontSize: 12, color: Colors.black54,),),
+          Material(
+            borderRadius: BorderRadius.circular(30),
+            elevation: 5,
+            color: Colors.lightBlueAccent,
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Text(
+                '$text',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
